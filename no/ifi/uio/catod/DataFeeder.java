629e458a0c0a2c2c42c33aca3973f64159497cb5
Cato Danielsen <cato.danielsen@rr-research.no>
2016-06-17 17:00:58 +0200

    initial

---
+++ no/ifi/uio/catod/DataFeeder.java
@@ -0,0 +1,20 @@
+package no.ifi.uio.catod;
+
+
+/**
+ * Small application for simulating sensor node delivering
+ * arbitrary data to main application
+ * @author catoda
+ *
+ */
+public class DataFeeder {
+
+	public static void main(String[] args) {
+		// TODO Create a non-blocking server socket:
+		
+		
+		
+		// TODO create thread for each connection. Read file and keep sending data
+	}
+
+}
---
+++ no/ifi/uio/catod/Feeder.java
@@ -0,0 +1,18 @@
+package no.ifi.uio.catod;
+
+public class Feeder implements Runnable {
+
+	@Override
+	public void run() {
+		
+		// TODO: Read file
+		
+		while (true) {
+			
+		}
+		
+		
+
+	}
+	
+}
