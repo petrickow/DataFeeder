835f313985ffd81db105738ee48060396b4ca865
Cato Danielsen <catodanielsen@gmail.com>
2016-08-01 21:02:40 +0200

    recatored: moved handle request into separate method

--- DataFeeder.java
+++ DataFeeder.java
@@ -5,6 +5,7 @@ import java.io.WriteAbortedException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -33,12 +34,13 @@ public class DataFeeder {
 	private static final String ABORTCODE = "400";
 	private static final String REQCODE = "REQ";
 	
-	
 	private static int bufferSize = 30;
 
 	private static ArrayList<Thread> threadList;
 	private static Map<String, Feeder> feederMap;
 
+	
+	
 	public static void main(String[] args) throws IOException {
 		
 		int port = 4444;
@@ -52,9 +54,10 @@ public class DataFeeder {
 		
 		Selector selector = Selector.open();
 		
+		// set property in the key that identifies the channel
 		SelectionKey socketServerSelectionKey = channel.register(selector,
                 SelectionKey.OP_ACCEPT);
-        // set property in the key that identifies the channel
+        
 		
 		/* Maps and lists of running threads, properties and of 
 		 * feeder instances TODO: Remove all references to feeder and thread
@@ -63,6 +66,7 @@ public class DataFeeder {
 		 */
 		threadList = new ArrayList<Thread>();
 		feederMap = new HashMap<String, Feeder>();
+		
 		Map<String, String> properties = new HashMap<String, String>();
         
         properties.put(channelType, serverChannel);
@@ -82,72 +86,82 @@ public class DataFeeder {
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 // the selection key could either by the socketserver meaning a new connection has been made, 
-                // or
-                // a socket client that is ready for read/write
+                // or a socket client that is ready for read/write
                 // we use the properties object attached to the channel to find the type of channel.
                 Map<String, String> propertiesMap = (Map<String, String>)  key.attachment();
                 
-                // INIT
-                if ((propertiesMap.get(channelType).equals(
-                        serverChannel))) {
-                	// MOVE TO initalConnection method
-                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key
-                            .channel(); // new channel
-                    SocketChannel clientSocketChannel = serverSocketChannel
-                            .accept();
-
-                    // DONE after setup, spawn thread and map channel to it
-                    if (clientSocketChannel != null) {
-                        clientSocketChannel.configureBlocking(false);
-                        SelectionKey clientKey = clientSocketChannel.register(
-                                selector, 
-                                SelectionKey.OP_READ,
-                                SelectionKey.OP_WRITE);
-                        
-                        Map<String, String> clientproperties = new HashMap<String, String>(); // attached on the key to be able to find stuff 
-                        
-//                    	System.out.println("New conneciton..."); //TODO: implement logging
-                        
-                        /* TODO: rewrite open channel/running threads mapping
-                         * Dette blir litt tullete, kan like så greit bare lage Feeder objektet og
-                         * mellomlagre det. Må uansett finne det frem og legge til filnavn når vi
-                         * får det, for så å starte tråden som serverer den filen.
-                         * Her kan vi redusere antall maps og unødvendig kompleksitet.
-                         */
-                        Feeder f = new Feeder(clientSocketChannel);
-                        Thread feeder = new Thread(f);
-                        
-                        if (threadList.add(feeder)) {
-                        	clientproperties.put("thread", Integer.toString(threadList.indexOf(feeder))); // map associated thread to index
-                        	feederMap.put(Integer.toString(threadList.indexOf(feeder)), f); // thread contains key to feeder object in map
-                        }
-
-                        clientproperties.put(channelType, clientChannel); // map channel type to client channel
-                        clientKey.attach(clientproperties);
- 
-//                      ACK to client
-                        CharBuffer buffer = CharBuffer.wrap("OK," + OKCODE);
-
-                        while (buffer.hasRemaining()) {
-                            clientSocketChannel.write(Charset.defaultCharset()
-                                    .encode(buffer));
-                        }
-
-                        buffer.clear();
-                    }
-                }     
-//				INIT COMPLETE, expect ACK or ABORT from client
-//				Otherwise it is probably connection lost. Other cases are not considered?
-                else {
+                if ((propertiesMap.get(channelType).equals(serverChannel))) {
+                	registerNewChannel(selector, key);
+                } else {
                     if (key.isReadable()) {
                     	readFromClient(key, propertiesMap);
+                    } else {
+//        				Otherwise it is probably connection lost. Other cases are not considered?
+                    	System.out.println("Key not readable");
                     }
                 }
-//              once a key is handled, it needs to be removed
+//			once a key is handled, it needs to be removed
             iterator.remove();
             }
         } while (true);
     }
+
+	
+	/** 
+	 * Set up new thread and Feeder object for the new connection. 
+	 * 
+	 * @param selector
+	 * @param key
+	 * @throws IOException
+	 * @throws ClosedChannelException
+	 */
+	private static void registerNewChannel(Selector selector, SelectionKey key)
+			throws IOException, ClosedChannelException {
+		// MOVE TO initalConnection method
+		ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key
+		        .channel(); // new channel
+		SocketChannel clientSocketChannel = serverSocketChannel
+		        .accept();
+
+		// DONE after setup, spawn thread and map channel to it
+		if (clientSocketChannel != null) {
+		    clientSocketChannel.configureBlocking(false);
+		    SelectionKey clientKey = clientSocketChannel.register(
+		            selector, 
+		            SelectionKey.OP_READ,
+		            SelectionKey.OP_WRITE);
+		    
+		    Map<String, String> clientproperties = new HashMap<String, String>(); // attached on the key to be able to find stuff 
+		    
+//                    	System.out.println("New conneciton..."); //TODO: implement logging
+		    
+		    /* TODO: rewrite open channel/running threads mapping
+		     * Dette blir litt tullete, kan like så greit bare lage Feeder objektet og
+		     * mellomlagre det. Må uansett finne det frem og legge til filnavn når vi
+		     * får det, for så å starte tråden som serverer den filen.
+		     * Her kan vi redusere antall maps og unødvendig kompleksitet.
+		     */
+		    Feeder f = new Feeder(clientSocketChannel);
+		    Thread feeder = new Thread(f);
+		    
+		    if (threadList.add(feeder)) {
+		    	clientproperties.put("thread", Integer.toString(threadList.indexOf(feeder))); // map associated thread to index
+		    	feederMap.put(Integer.toString(threadList.indexOf(feeder)), f); // thread contains key to feeder object in map
+		    }
+
+		    clientproperties.put(channelType, clientChannel); // map channel type to client channel
+		    clientKey.attach(clientproperties);
+ 
+		    CharBuffer buffer = CharBuffer.wrap("OK," + OKCODE); // ACK to client
+
+		    while (buffer.hasRemaining()) {
+		        clientSocketChannel.write(Charset.defaultCharset()
+		                .encode(buffer));
+		    }
+
+		    buffer.clear();
+		}
+	}
 	
 	/**
 	 * Read the response from client, called when activity is detected on channel.
@@ -171,23 +185,14 @@ public class DataFeeder {
 //	            TODO: refactor out of this method --> handleAck() 
 		        if (resp.endsWith(REQCODE)) { // had to remove \n with trim()
 //		        	System.out.print("Got 200\t");
-		
-		        	Feeder f = feederMap.get(propertiesMap.get("thread"));
-		        	Thread thf = threadList.get(Integer.valueOf(propertiesMap.get("thread"))); 
-		        	
-		        	String[] r = resp.split(delimiter);
-		        	
-		        	switch (r.length) {
-		        		case 1: System.out.println("--DataFeeder->\tReturn file list"); writeToClient(getFiles(), clientCh); break;			        		
-		        		case 2: System.out.println("--DataFeeder->\tOK, requested file: " + r[0]); f.setFileName(r[0]); thf.start();  break;
-		        		case 3: System.out.println("--DataFeeder->\tTODO: send " + r[2] + " times, instead of infinite");
-		        		default: System.out.println("--DataFeeder->\tERROR: unknown request size?\t " + resp); break;
-		        	}
-		        } else if (resp.endsWith(ABORTCODE)) {
+		        	handleRequest(propertiesMap.get("thread"), clientCh, resp);
+		        } 
+		        else if (resp.endsWith(ABORTCODE)) {
 		        	System.out.format("--DataFeeder->\tABORT: Got abort code:\t" + resp);
 		        	clientCh.close();
 		        	key.cancel(); // ??? 
-		        } else {
+		        } 
+		        else {
 		        	System.out.format("--DataFeeder->\tERROR: Got unknown code\t %s\n", resp);
 		        	//TODO: write back on close
 		        }
@@ -208,6 +213,32 @@ public class DataFeeder {
         }
         return bytesRead;
 	}
+
+	/**
+	 * Parses and acts upon client request.
+	 * @param threadKey - key to feeder and thread map
+	 * @param clientChannel
+	 * @param clientResponse
+	 */
+	private static void handleRequest(String threadKey, SocketChannel clientChannel, String clientResponse) {
+		Feeder f = feederMap.get(threadKey);
+		Thread thf = threadList.get(Integer.valueOf(threadKey)); 
+		
+		String[] r = clientResponse.split(delimiter);
+		
+		switch (r.length) {
+			case 1: System.out.println("--DataFeeder->\tReturn file list"); writeToClient(getFiles(), clientChannel); break;			        		
+			case 2: System.out.println("--DataFeeder->\tOK, requested file: " + r[0]); f.setFileName(r[0]); thf.start();  break;
+			case 3: System.out.println("--DataFeeder->\tTODO: send " + r[2] + " times, instead of infinite");
+			default: System.out.println("--DataFeeder->\tERROR: unknown request size?\t " + clientResponse); break;
+		}
+	}
+	
+	/**
+	 * Writes the toSend string to the client via socket.
+	 * @param toSend
+	 * @param ch
+	 */
 	private static void writeToClient(String toSend, SocketChannel ch) {
     	CharBuffer buffer = CharBuffer.wrap(toSend);
         while (buffer.hasRemaining()) {
